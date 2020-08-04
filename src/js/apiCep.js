8b66793e7212589e339f0ecab44b1d04fb3d2c87
matheusmartins <matheusgleiciel@hotmail.com>
2020-08-04 12:19:05 -0300

    commit

--- src/js/apiCep.js
+++ src/js/apiCep.js
@@ -1,11 +1,19 @@
 $(document).ready(function() {
 
+    $("#rua").css("display", "none");
+    $("#rua1").css("display", "none");
+    $("#bairro").css("display", "none");
+    $("#bairro1").css("display", "none");
+    $("#cidade").css("display", "none");
+    $("#cidade1").css("display", "none");
+    $("#uf").css("display", "none");
+    $("#uf1").css("display", "none");
+
     function limpa_formulário_cep() {
         $("#rua").val("");
         $("#bairro").val("");
         $("#cidade").val("");
         $("#uf").val("");
-        $("#ibge").val("");
     }
     
     $("#cep").blur(function() {
@@ -22,34 +30,30 @@ $(document).ready(function() {
                 $("#bairro").val("espera um pouquinho");
                 $("#cidade").val("espera um pouquinho");
                 $("#uf").val("espera um pouquinho");
-                $("#ibge").val("espera um pouquinho");
     
                 $.getJSON("https://viacep.com.br/ws/"+ cep +"/json/?callback=?", function(dados) {
     
                     if (!("erro" in dados)) {
-                        $("#rua").val(dados.logradouro + dados.complemento);
-                        $("#bairro").val(dados.bairro);
-                        $("#cidade").val(dados.localidade);
-                        $("#uf").val(dados.uf);
-                        $("#ibge").val(dados.ibge);
+                        $("#rua").val(dados.logradouro + dados.complemento).css("display", "block");
+                        $("#bairro").val(dados.bairro).css("display", "block");
+                        $("#cidade").val(dados.localidade).css("display", "block");
+                        $("#uf").val(dados.uf).css("display", "block");
                     }
                     else {
                         limpa_formulário_cep();
-                        $("#rua").val("cep não encontrado");
-                        $("#bairro").val("cep não encontrado");
-                        $("#cidade").val("cep não encontrado");
-                        $("#uf").val("cep não encontrado");
-                        $("#ibge").val("cep não encontrado");
+                        $("#rua").val("Cep não encontrado").setinterval(css("display", "block"));
+                        $("#bairro").val("Cep não encontrado").css("display", "block");
+                        $("#cidade").val("Cep não encontrado").css("display", "block");
+                        $("#uf").val("Cep não encontrado").css("display", "block");
                     }
                 });
             }
             else {
                 limpa_formulário_cep();
-                  $("#rua").val("cep inválido");
-                  $("#bairro").val("cep inválido");
-                  $("#cidade").val("cep inválido");
-                  $("#uf").val("cep inválido");
-                  $("#ibge").val("cep inválido");
+                  $("#rua").val("Cep inválido").css("display", "block");
+                  $("#bairro").val("Cep inválido").css("display", "block");
+                  $("#cidade").val("Cep inválido").css("display", "block");
+                  $("#uf").val("Cep inválido").css("display", "block");
             }
         }
         else {
